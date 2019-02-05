67af2abae1a2656dab38e39262b6330a6d1a7f29
dtan_yh85 <darren243@hotmail.com>
2019-02-05 18:28:09 +0800

    Video 135 - Make Projectile Move

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed = 1f;
+    [SerializeField] float rotateSpeed = 900f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(-Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        transform.position += Vector3.right * projectileSpeed * Time.deltaTime;
+    }
+}
