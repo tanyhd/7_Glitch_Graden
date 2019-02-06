94ce45f6a5d5f7e1b60ea97cda67e764a085a34f
dtan_yh85 <darren243@hotmail.com>
2019-02-06 14:17:22 +0800

    Video 140 - Spawn At Mouse Position

--- Assets/Scripts/DefendersSpawner.cs
+++ Assets/Scripts/DefendersSpawner.cs
@@ -9,12 +9,19 @@ public class DefendersSpawner : MonoBehaviour
 
     private void OnMouseDown()
     {
-        SpawnDefender();
+        SpawnDefender(GetSquareClicked());
         
     }
 
-    private void SpawnDefender()
+    private void SpawnDefender(Vector2 worldPos)
     {
-        GameObject newDefender = Instantiate(defenderPrefab, transform.position, Quaternion.identity) as GameObject;
+        GameObject newDefender = Instantiate(defenderPrefab, worldPos, Quaternion.identity) as GameObject;
+    }
+
+    private Vector2 GetSquareClicked()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
     }
 }
