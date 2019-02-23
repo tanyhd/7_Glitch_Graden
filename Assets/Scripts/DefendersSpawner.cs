3c475b49f27e880e4cdb540dee84f64c63d0b72c
dtan_yh85 <darren243@hotmail.com>
2019-02-23 16:11:44 +0800

    Video 170 - Tidy Hierarchy = Tidy Mind

--- Assets/Scripts/DefendersSpawner.cs
+++ Assets/Scripts/DefendersSpawner.cs
@@ -6,6 +6,22 @@ using UnityEngine;
 public class DefendersSpawner : MonoBehaviour
 {
     Defender defender;
+    GameObject defenderParent;
+    const string DEFENDER_PARENT_NAME = "Defenders";
+
+    private void Start()
+    {
+        CreateDefenderParent();
+    }
+
+    private void CreateDefenderParent()
+    {
+        defenderParent = GameObject.Find(DEFENDER_PARENT_NAME);
+        if (!defenderParent)
+        {
+            defenderParent = new GameObject(DEFENDER_PARENT_NAME);
+        }
+    }
 
     private void OnMouseDown()
     {
@@ -33,6 +49,7 @@ public class DefendersSpawner : MonoBehaviour
     {
         if (defender == null) { return; }
         Defender newDefender = Instantiate(defender, roundedPos, Quaternion.identity) as Defender;
+        newDefender.transform.parent = defenderParent.transform;
     }
 
     private Vector2 GetSquareClicked()
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,14 +7,27 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
-    Animator animator;
 
+    Animator animator;
+    GameObject projectileParent;
     AttackerSpawner myLaneSpawner;
+
+    const string PROJECTILE_PARENT_NAME = "Projectiles";
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         SetLaneSpawner();
+        CreateProjectileParent();
+    }
+
+    private void CreateProjectileParent()
+    {
+        projectileParent = GameObject.Find(PROJECTILE_PARENT_NAME);
+        if (!projectileParent)
+        {
+            projectileParent = new GameObject(PROJECTILE_PARENT_NAME);
+        }
     }
 
     private void SetLaneSpawner()
@@ -56,7 +69,8 @@ public class Shooter : MonoBehaviour
 
     public void Fire()
     {
-        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+        GameObject newProjectile = Instantiate(projectile, gun.transform.position, Quaternion.identity) as GameObject;
+        newProjectile.transform.parent = projectileParent.transform;
     }
 
 }
